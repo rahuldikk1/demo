488927bf9722bb3d66229c25013679dcffe5b9cb
rahul <rahulsabu00018@gmail.com>
2023-01-03 23:13:10 +0530

    add

--- rahulapp/urls.py
+++ rahulapp/urls.py
@@ -4,9 +4,6 @@ from  .import views
 urlpatterns = [
     path('', views.add, name='add'),
     path('delete/<int:id>/',views.delete,name='delete'),
-    path('update/<int:id>/', views.update, name='update'),
-    path('cbvhome/',views.Tasklistview.as_view(),name='cbvhome'),
-    path('cbvdetail/<int:pk>/',views.TaskDetailview.as_view(),name='cbvdetail'),
-    path('cbvupdate/<int:pk>/',views.Taskupdateview.as_view(),name='cbvupdate'),
-    path('cbvdelete/<int:pk>/',views.TaskDeleteView.as_view(),name='cbvdelet')
+    path('update/<int:id>/', views.update, name='update')
+
 ]
